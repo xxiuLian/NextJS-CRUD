58bd41c2f1da6af666c1400e8e454fd306f86654
syeony <syeony@mz.co.kr>
2023-05-10 15:09:18 +0900

    env

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,13 +5,12 @@ import GoogleProvider from "next-auth/providers/google";
 export const authOptions = {
   providers: [
     GithubProvider({
-      clientId: "a4da0b062fe2ed8a9679",
-      clientSecret: "7a921441c4c266dabaa8b3fe8d2a69ed9254ee06",
+      clientId: process.env.GIT_ID,
+      clientSecret: process.env.GIT_SECRET,
     }),
     GoogleProvider({
-      clientId:
-        "180566327522-mlq16pqrbg2jcogefmt55tltibjnt2ap.apps.googleusercontent.com",
-      clientSecret: "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",
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
   secret: "zaqxswcdev123frbgtn456hymju,ki789.lo/;p",
